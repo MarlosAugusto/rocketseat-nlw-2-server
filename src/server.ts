1f80cc57459f27b3bda685ae3ab5ff79aed54dc5
MarlosAugusto <marlos.gomes@codengage.com>
2020-08-05 08:45:29 -0300

    feat: using Rest Client

--- src/server.ts
+++ src/server.ts
@@ -2,7 +2,10 @@ import express from 'express'
 
 const app = express()
 
-app.get('/users', () => {
-  console.log('Acessou a rota /users')
+app.use(express.json())
+
+app.get('/', (request, response) => {
+
+  return response.json({ message: 'Hello world!' })
 })
 app.listen(3333)
